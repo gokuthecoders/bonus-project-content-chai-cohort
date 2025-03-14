abdef468bfaa571db15eb9a0154f80c167b73abe
VISHAL KUMAR <vishalkr12012001@gmail.com>
2025-03-14 17:47:15 +0530

    simple-calculator

---
+++ 05-calculator/simple-calculator/script.js
@@ -0,0 +1,39 @@
+const x = document.getElementById('number1')
+const y = document.getElementById('number2')
+
+const result = document.getElementById('result')
+
+function calculate(operator){
+    const num1 = parseInt(x.value);
+    const num2 = parseInt(y.value);
+
+    if(isNaN(num1) || isNaN(num2)){
+        result.innerText = 'Please enter a valid number'
+        return
+    }
+
+    switch(operator){
+        case '+': {
+            result.innerText = `Sum: ${num1 + num2}`
+        }
+        break
+        case '-': {
+            result.innerText = `Sub: ${num1 - num2}`
+        }
+        break
+        case '*': {
+            result.innerText = `Mul: ${num1 * num2}`
+        }
+        break
+        case '/': {
+        result.innerText = `Mul: ${num1 / num2}`
+        }
+        break
+        case '%': {
+            result.innerText = `Mul: ${num1 % num2}`
+        }
+        break
+        default : 
+        result.innerText = 'Invalid Operator'
+    }
+}
